131a45c431d232c59f17b799c21102840065a52a
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-05-26 17:38:13 +0200

    Add files via upload

---
+++ Level_11/Javascript_02/script.js
@@ -0,0 +1,16 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+// deze comment is in Node niet zichtbaar 
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+let division = 637263 / 54;
+console.log(division);
+/* met block comments worden meerdere alineas uitgeschakeld.
+daardoor is het resultaat nog de comment in Node zichtbaar */
+// let multiplication = 1230941 * 1823794;
+// console.log(multiplication);
+
+// let division = 637263 / 54;
+// console.log(division);
