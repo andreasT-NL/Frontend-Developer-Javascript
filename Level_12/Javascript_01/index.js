c37352c8b0d393b75bf8c9ae7b93d60f171754f6
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-05-27 13:23:15 +0200

    Add files via upload

---
+++ Level_12/Javascript_01/index.js
@@ -0,0 +1,25 @@
+let firstname = 'andreas';
+console.log(firstname);
+
+let sum = 2 + 2 + 1;
+console.log("sum:", sum);
+
+let Sum = '2' + '2';
+console.log('Sum', Sum);
+
+let subtr = 1 - 5;
+console.log("subtr:", subtr);
+let multipl = 3 * 4;
+console.log('multipl:', multipl);
+
+let division = 15 / 3;
+console.log('division:', division);
+
+let modulus = 12 % 9;
+console.log('modulus:', modulus);
+
+let age = 48;
+console.log(typeof age);
+
+let ageAsString = "35";
+console.log(typeof ageAsString);
---
+++ Level_12/Javascript_02/script.js
@@ -0,0 +1,17 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+// deze comment is in Node niet zichtbaar 
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+let division = 637263 / 54;
+console.log(division);
+/* met block comments worden meerdere alineas uitgeschakeld.
+daardoor is het resultaat nog de comment in Node zichtbaar */
+// let multiplication = 1230941 * 1823794;
+// console.log(multiplication);
+
+//zo kan het dus ook
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
