9b918db5a9eb6b41c289591d68403543ee604e11
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-05-27 13:23:41 +0200

    Add files via upload

---
+++ Level_13/Javascript_01/thebar.js
@@ -0,0 +1,26 @@
+//opdracht 1
+const age = 22;
+if (age >= 18) {
+    console.log(age);
+    document.write('you are in');
+} if (age < 18) {
+    document.write('sorry you are too young');
+}
+//opdracht 2
+const isFemale = true;
+if (isFemale) {
+    console.log('female');
+    document.write('hello laydies');
+} else {
+    console.log('male');
+    document.write('sorry guys, not tonight');
+}
+//opdracht 3
+const driverStatus = 'no';
+if (driverStatus == 'bob') {
+    console.log('have a safe trip!');
+    document.write('have a safe trip!');
+} else {
+    console.log('get a cab');
+    document.write('get a cab');
+}
