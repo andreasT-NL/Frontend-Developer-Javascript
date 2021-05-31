fc6fd7464849897e4a0d5e2704bf42b84295fe7f
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-05-31 14:12:17 +0200

    Add files via upload

---
+++ Level_14/Javascript_01/function.js
@@ -0,0 +1,20 @@
+const makePizza = function () {
+    console.log('make dogh');
+    console.log('make tomato sauce');
+    console.log('heat the oven to 200 degrees');
+    console.log('chup fresh ingredients');
+    console.log('compose your pizza');
+    console.log('put in the oven till ready');
+}
+
+
+const makeSushi = function () {
+    console.log('make rice');
+    console.log('cut fish and vegies');
+    console.log('roll fish and rice');
+}
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
